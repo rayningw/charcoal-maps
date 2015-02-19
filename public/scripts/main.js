93c006cad285219beee7a85801f2121eb8416ea4
Ray Wu <rwu@quantcast.com>
2015-02-19 01:14:49 -0800

    Add presets and control panel bar

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,48 @@
-var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
-var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
-var rockridge = new google.maps.LatLng(37.8446, -122.2512);
-var stockholm = new google.maps.LatLng(59.335423, 18.069600);
-var manhattan = new google.maps.LatLng(40.790651, -73.954983);
-var wentworthville = new google.maps.LatLng(-33.807066, 150.972452);
-
+var DEFAULT_PRESETS = {
+  rockridge: {
+    latitude: 37.8446,
+    longitude: -122.2512
+  },
+  pyrmont: {
+    latitude: -33.8665433,
+    longitude: 151.1956316
+  },
+  sanFrancisco: {
+    latitude: 37.774546,
+    longitude: -122.433523
+  },
+  stockholm: {
+    latitude: 59.335423,
+    longitude: 18.069600
+  },
+  manhattan: {
+    latitude: 40.790651,
+    longitude: -73.954983
+  },
+  wentworthville: {
+    latitude: -33.807066,
+    longitude: 150.972452
+  }
+};
+  
 var App = React.createClass({
+
+  getInitialState: function() {
+    return {
+      presets: DEFAULT_PRESETS,
+      initialLocation: DEFAULT_PRESETS.rockridge
+    }
+  },
 
   handleSearch: function(address) {
     this.addressSearchNotifier.listeners.forEach(function(listener) {
       listener(address);
+    });
+  },
+  
+  handleSelectPreset: function(preset) {
+    this.locationJumpNotifier.listeners.forEach(function(listener) {
+      listener(preset);
     });
   },
 
@@ -21,11 +54,23 @@ var App = React.createClass({
     }
   },
 
+  locationJumpNotifier: {
+    listeners: [],
+    subscribe: function(listener) {
+      // TODO(rwu): Return an unsubscribe function.
+      this.listeners.push(listener);
+    }
+  },
+
   render: function() {
     return (
       <div id="app">
-        <ControlPanel onSearch={this.handleSearch} />
-        <MapPanel addressSearchNotifier={this.addressSearchNotifier} />
+        <ControlPanel presets={this.state.presets}
+                      onSearch={this.handleSearch}
+                      onSelectPreset={this.handleSelectPreset} />
+        <MapPanel addressSearchNotifier={this.addressSearchNotifier}
+                  locationJumpNotifier={this.locationJumpNotifier}
+                  initialLocation={this.state.initialLocation} />
       </div>
     );
   }
@@ -36,16 +81,40 @@ var App = React.createClass({
 var ControlPanel = React.createClass({
 
   handleSearch: function(event) {
+    event.preventDefault();
     // TODO(rwu): Get address using virtual DOM.
     var address = this.refs.address.getDOMNode().value.trim();
     this.props.onSearch(address);
   },
 
+  handleSelectChange: function(event) {
+    var name = event.target.value;
+    this.props.onSelectPreset(this.props.presets[name]);
+    // TODO(rwu): Get address using virtual DOM.
+    this.refs.address.getDOMNode().value = '';
+    event.target.selectedIndex = 0;
+  },
+
   render: function() {
+    var presetsNodes = Object.keys(this.props.presets).map(function(name) {
+      var preset = this.props.presets[name];
+      return (
+        <option value={name}>{name}</option>
+      );
+    }.bind(this));
+
     return (
       <div id="panel">
-        <input ref="address" id="address" type="textbox"></input>
-        <input type="button" value="Search" onClick={this.handleSearch}></input>
+        <div id="panel-title">Charcoal Maps</div>
+        <select onChange={this.handleSelectChange}>
+          <option>Select preset...</option>
+          {presetsNodes}
+        </select>
+        <div>or</div>
+        <form onSubmit={this.handleSearch}>
+          <input ref="address" id="address" type="textbox" placeholder="Enter location..."></input>
+          <input type="submit" value="Search"></input>
+        </form>
       </div>
     );
   }
@@ -57,6 +126,7 @@ var MapPanel = React.createClass({
   componentDidMount: function() {
     this.initializeMap();
     this.props.addressSearchNotifier.subscribe(this.handleAddressSearch);
+    this.props.locationJumpNotifier.subscribe(this.handleLocationJump);
   },
 
   shouldComponentUpdate: function(nextProps, nextState) {
@@ -81,6 +151,11 @@ var MapPanel = React.createClass({
     this.mapState.geocoder.geocode({'address': address}, this.handleGeocodeCallback);
   },
 
+  handleLocationJump: function(location) {
+    var latLng = new google.maps.LatLng(location.latitude, location.longitude);
+    this.mapState.map.setCenter(latLng);
+  },
+
   handleGeocodeCallback: function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       this.mapState.map.setCenter(results[0].geometry.location);
@@ -91,8 +166,11 @@ var MapPanel = React.createClass({
   },
 
   initializeMap: function() {
+    var initialLatLng = new google.maps.LatLng(
+        this.props.initialLocation.latitude,
+        this.props.initialLocation.longitude);
     this.mapState.map = new google.maps.Map(document.getElementById('map-canvas'), {
-      center: rockridge,
+      center: initialLatLng,
       zoom: 15
     });
 
